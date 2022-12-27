e65aba378ec5b6c7e0c5a3ddbd8997c8c0eb54e3
Kris Egidio <krisegidio@gmail.com>
2022-12-27 20:54:27 -0300

    feat: project setup and model person

---
+++ src/main/java/rpa/people/generator/Main.java
@@ -0,0 +1,14 @@
+package rpa.people.generator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import rpa.people.generator.model.Person;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//
+	}
+
+}
